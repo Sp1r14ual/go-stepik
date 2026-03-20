dd050d27d1d0d291e85523a040015001aa27d1db
Sp1r14ual <73416809+Sp1r14ual@users.noreply.github.com>
2026-03-21 02:24:04 +0700

    Add files via upload

---
+++ hw1_tree/main.go
@@ -0,0 +1,60 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"io"
+// 	"os"
+// 	"path/filepath"
+// 	"strings"
+// )
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+)
+
+var depth int
+
+func dirTree(out *os.File, path string, printFiles bool) (error) {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	sort.Slice(files, func(i, j int) bool {
+		return files[i].Name() < files[j].Name() // Use '<' for ascending
+	})
+
+	for _, file := range files {
+		for i := 0; i < depth; i++ {
+			fmt.Print("│\t")
+		}
+
+		fmt.Println("├───" + file.Name())
+		if file.IsDir() {
+			depth++
+			childFolder := filepath.Join(path, file.Name())
+			dirTree(out, childFolder, printFiles)
+		}
+		if file == files[len(files)-1]{
+			fmt.Println("└───" + file.Name())
+		}
+	}
+	depth--
+	return nil
+}
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
